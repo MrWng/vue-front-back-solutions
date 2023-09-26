2ac352f19b076e1f3815c6fef866f04978c15d1a
wangyi <wangyiz@digiwin.com>
2023-09-26 14:28:18 +0800

    feat: 配置接口本地代理

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,16 @@ function resolve(dir) {
 }
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '/api': {
+        // target: 'https://api.imooc-admin.lgdsunday.club', // 跨域接口的地址
+        target: 'http://127.0.0.1:8800', // 跨域接口的地址
+        changeOrigin: true
+      }
+    }
+  },
+
   chainWebpack(config) {
     // 设置 svg-sprite-loader
     config.module.rule('svg').exclude.add(resolve('src/icons')).end()
